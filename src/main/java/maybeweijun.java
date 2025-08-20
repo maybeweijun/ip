cc5ab751504c0360b74f6ca14c830b0c92e80aae
maybeweijun <weijunwork@gmail.com>
2025-08-20 14:08:25 +0800

    Rename Duke.java to maybeweijun.java and implemented skeletal version that greets users and exits

---
+++ src/main/java/maybeweijun.java
@@ -0,0 +1,16 @@
+public class maybeweijun {
+    public static void main(String[] args) {
+        String logo = "[m-a-y-b-e-w-e-i-j-u-n]";
+	System.out.println("Hello from\n" + logo+"\n");
+	query();
+	exit();
+    }
+    public static void query() {
+	    System.out.println("What can I do for you?\n");
+    }
+
+    public static void exit() {
+	    System.out.println("Bye. Hope to see you again soon!\n");
+    }
+
+}
