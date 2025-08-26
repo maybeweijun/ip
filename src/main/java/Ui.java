529d2bdf938877dd7dbc7af74f8356e5e8ad60fe
maybeweijun <weijunwork@gmail.com>
2025-08-27 00:27:31 +0800

    Add Ui.java, edit other classes to fit Ui.java

---
+++ src/main/java/Ui.java
@@ -0,0 +1,46 @@
+public class Ui {
+    public static void printLogo() {
+        String logo = "[m-a-y-b-e-w-e-i-j-u-n]";
+        System.out.println("Hello from\n" + logo + "\n");
+    }
+
+    public static void printQuery() {
+        System.out.println("What can I do for you? \n");
+    }
+
+    public static void printTaskList(TaskList tasks) {
+        System.out.println("-----------");
+        for (int i = 0; i < tasks.size(); i++) {
+            System.out.println((i + 1) + ". " + tasks.get(i));
+        }
+        System.out.println("-----------\n");
+    }
+
+    public static void printTaskAdded(Task task) {
+        System.out.println("-----------\nadded: " + task + "\n-----------\n");
+    }
+
+    public static void printMarked(int oneBasedIndex, Task task) {
+        System.out.println("Marked task " + oneBasedIndex + " as done.");
+        System.out.println(task);
+    }
+
+    public static void printUnmarked(int oneBasedIndex, Task task) {
+        System.out.println("Unmarked task " + oneBasedIndex + ".");
+        System.out.println(task);
+    }
+
+    public static void printDeleted(Task removedTask, int remainingCount) {
+        System.out.println("Noted. I've removed this task:");
+        System.out.println(removedTask);
+        System.out.println("Now you have " + remainingCount + " tasks in the list.");
+    }
+
+    public static void printExit() {
+        System.out.println("Bye. Hope to see you again soon!\n");
+    }
+
+    public static void printError(String message) {
+        System.out.println(message);
+    }
+}
